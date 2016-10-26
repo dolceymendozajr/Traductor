139a7e37012f2c4ed59ee80732a3082c185d5df9
Wilson Tovar <twilson@uninorte.edu.co>
2016-10-26 17:48:05 -0500

    Metodo propio :)

--- src/lab_final_dolcey_wilson/MetodoEncript.java
+++ src/lab_final_dolcey_wilson/MetodoEncript.java
@@ -10,7 +10,7 @@ public class MetodoEncript {
 //    160 - 163 (á)
 //    65 67 68 69 70 71 72 73 74 75 76 78 80 83 84 86 93 95 98 99 100 102 105 107 110 114 120 121 122 152 153 161 163 
     static int num[] = new int[32];
-    static char abc[] = {'n', 'H', 'a', 'e', 'Q', 'O','R', 'r', 'u', 'Z', 'o','ñ', 'N', 'h', 'J', 'i','p', 'I'};
+    static char abc[] = {'n', 'H', 'D','a', 'e', 'Q', 'F','b', 'w', 's','v','O', 'y', 'R', 'S','r', 'u', 'Z', 'o','ñ', 'x','N', 'h', 'U' ,'J', 'W','i','p', 'I', 'c', 'B', 'f'};
     static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
@@ -36,12 +36,9 @@ public class MetodoEncript {
         dicc[30] = 'ó';
         dicc[31] = 'ú';
         
-        System.out.println("TAM: " + abc.length);
         // BLOQUE 2 - GENERAR VECTOR DE NUMEROS
-        for (int i = 0; i < num.length; i++) {
+        for (int i = 0; i < num.length; i++)
             num[i] = (int) abc[i];
-            System.out.println(num[i]);
-        }
     }
 
     // FUNCIÓN PARA ENCRIPTRAR
@@ -58,8 +55,6 @@ public class MetodoEncript {
             cod = cod + (char) a;
         }
 
-        System.out.println(frase + " / " + cod);
-        System.out.println((char) a);
         return cod;
     }
 }
