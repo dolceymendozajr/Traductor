542ef73d542f6bdae6abaa3fd226afa716dcb75f
Wilson Tovar <twilson@uninorte.edu.co>
2016-10-26 10:07:12 -0500

    Agregada la clase para la conversión a Binario

---
+++ src/lab_final_dolcey_wilson/Binario.java
@@ -0,0 +1,14 @@
+package lab_final_dolcey_wilson;
+
+public class Binario {
+    public String Convertir(String palabra) {
+        String text_convertido = "";
+        
+        for (int i = 0; i < palabra.length(); i++) {
+            
+        }
+        
+        
+        return text_convertido;
+    }
+}
