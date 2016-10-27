05fc8c730c932e52da517833400de4f2ee636802
Dolcey Mendoza Anaya <dolceymendozajr@gmail.com>
2016-10-27 09:32:47 -0500

    Cambio de fuente en el formulario y cambio de posición en el botón siguiente

--- src/lab_final_dolcey_wilson/Cifrado.java
+++ src/lab_final_dolcey_wilson/Cifrado.java
@@ -38,21 +38,26 @@ public class Cifrado extends javax.swing.JFrame {
             }
         });
 
+        jLabel1.setFont(new java.awt.Font("Microsoft Yi Baiti", 1, 18)); // NOI18N
         jLabel1.setText("PALABRA");
 
+        jLabel2.setFont(new java.awt.Font("Microsoft Yi Baiti", 1, 16)); // NOI18N
         jLabel2.setText("PROPIO");
 
+        jLabel3.setFont(new java.awt.Font("Microsoft Yi Baiti", 1, 16)); // NOI18N
         jLabel3.setText("VIGENÉRE");
 
-        jLabel4.setText("PROPIO > VIGENERE");
+        jLabel4.setFont(new java.awt.Font("Microsoft Yi Baiti", 1, 16)); // NOI18N
+        jLabel4.setText("PROPIO + VIGENÉRE");
 
         txt_Propio.setEditable(false);
-        txt_Propio.setText(":)");
+        txt_Propio.setFont(new java.awt.Font("Microsoft Yi Baiti", 0, 16)); // NOI18N
 
-        jLabel5.setText("PROPIO > VIGENERE > BINARIO");
+        jLabel5.setFont(new java.awt.Font("Microsoft Yi Baiti", 1, 16)); // NOI18N
+        jLabel5.setText("PROPIO + VIGENÉRE + BINARIO");
 
         txt_Palabra.setEditable(false);
-        txt_Palabra.setText(":)");
+        txt_Palabra.setFont(new java.awt.Font("Microsoft Yi Baiti", 0, 16)); // NOI18N
         txt_Palabra.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 txt_PalabraActionPerformed(evt);
@@ -60,15 +65,16 @@ public class Cifrado extends javax.swing.JFrame {
         });
 
         txt_Propio_Vigenere.setEditable(false);
-        txt_Propio_Vigenere.setText(":)");
+        txt_Propio_Vigenere.setFont(new java.awt.Font("Microsoft Yi Baiti", 0, 16)); // NOI18N
 
         txt_Vigenere.setEditable(false);
-        txt_Vigenere.setText(":)");
+        txt_Vigenere.setFont(new java.awt.Font("Microsoft Yi Baiti", 0, 16)); // NOI18N
 
         txt_Propio_Vigenere_BInario.setEditable(false);
-        txt_Propio_Vigenere_BInario.setText(":)");
+        txt_Propio_Vigenere_BInario.setFont(new java.awt.Font("Microsoft Yi Baiti", 0, 16)); // NOI18N
 
-        btn_Siguiente.setText("SIG. >");
+        btn_Siguiente.setFont(new java.awt.Font("Microsoft Yi Baiti", 1, 18)); // NOI18N
+        btn_Siguiente.setText("SIGUIENTE PALABRA");
         btn_Siguiente.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 btn_SiguienteActionPerformed(evt);
@@ -81,34 +87,32 @@ public class Cifrado extends javax.swing.JFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addComponent(jLabel1)
-                            .addComponent(jLabel2)
-                            .addComponent(jLabel3)
-                            .addComponent(jLabel4)
-                            .addComponent(jLabel5)
-                            .addComponent(txt_Propio, javax.swing.GroupLayout.DEFAULT_SIZE, 374, Short.MAX_VALUE)
-                            .addComponent(txt_Vigenere)
-                            .addComponent(txt_Propio_Vigenere)
-                            .addComponent(txt_Propio_Vigenere_BInario))
-                        .addContainerGap(16, Short.MAX_VALUE))
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(txt_Palabra, javax.swing.GroupLayout.PREFERRED_SIZE, 251, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(btn_Siguiente)
-                        .addGap(25, 25, 25))))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jLabel2)
+                    .addComponent(jLabel3)
+                    .addComponent(jLabel4)
+                    .addComponent(jLabel5)
+                    .addComponent(txt_Propio, javax.swing.GroupLayout.DEFAULT_SIZE, 374, Short.MAX_VALUE)
+                    .addComponent(txt_Vigenere)
+                    .addComponent(txt_Propio_Vigenere)
+                    .addComponent(txt_Propio_Vigenere_BInario)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(jLabel1)
+                        .addGap(154, 154, 154))
+                    .addComponent(txt_Palabra))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(52, 52, 52)
+                .addComponent(btn_Siguiente, javax.swing.GroupLayout.PREFERRED_SIZE, 282, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 62, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jLabel1)
                 .addGap(13, 13, 13)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(txt_Palabra, javax.swing.GroupLayout.PREFERRED_SIZE, 48, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(btn_Siguiente))
+                .addComponent(jLabel1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(txt_Palabra, javax.swing.GroupLayout.PREFERRED_SIZE, 48, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jLabel2)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
@@ -125,20 +129,23 @@ public class Cifrado extends javax.swing.JFrame {
                 .addComponent(jLabel5)
                 .addGap(18, 18, 18)
                 .addComponent(txt_Propio_Vigenere_BInario, javax.swing.GroupLayout.PREFERRED_SIZE, 24, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(23, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 20, Short.MAX_VALUE)
+                .addComponent(btn_Siguiente)
+                .addGap(15, 15, 15))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
-
-    private void btn_SiguienteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_SiguienteActionPerformed
-    }//GEN-LAST:event_btn_SiguienteActionPerformed
 
     private void txt_PalabraActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txt_PalabraActionPerformed
     }//GEN-LAST:event_txt_PalabraActionPerformed
 
     private void formWindowClosed(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_formWindowClosed
     }//GEN-LAST:event_formWindowClosed
+
+    private void btn_SiguienteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_SiguienteActionPerformed
+
+    }//GEN-LAST:event_btn_SiguienteActionPerformed
 
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
